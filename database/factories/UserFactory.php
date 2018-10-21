4a49e4f2a22c0a6c9c1e1b88c0d24e87404c6cea
server <johnalexandere@hotmail.com>
2018-10-21 14:42:33 -0300

    users factory changes

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,10 +17,10 @@ $factory->define(App\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'password' => $faker->password,
         'remember_token' => str_random(10),
-        'first_name' => name,
-        'last_name' => lastName,
-        'client_address'=>address
+        'first_name' => $faker->word,
+        'last_name' => $faker->lastName,
+        'client_address'=>$faker->address
     ];
 });
